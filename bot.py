32fb88a0a51c3234838c428c581f318bac194c67
Tyler Nijmeh <tylernij@gmail.com>
2021-05-11 21:25:51 -0700

    Random emote thing
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- bot.py
+++ bot.py
@@ -25,8 +25,14 @@ class QueQue(discord.Client):
                 args is not None and \
                 not self.queue.full():
             await message.add_reaction(random_emote())
-            for _ in range(0, 100):
+            for _ in range(0, 10):
                 self.queue.put(args)
+        
+        if cmd == f'{P}mqe' and \
+                not self.queue.full():
+            await message.add_reaction(random_emote())
+            for _ in range(0, 10):
+                self.queue.put(random_emote())
 
         if cmd == f'{P}dq' and \
                 not self.queue.empty():
